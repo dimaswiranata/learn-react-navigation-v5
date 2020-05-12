a9954f370d4fb6a3301c21f496dc09ced1b9a930
Dimas Agusta Wiranata <dimaswiranata8@gmail.com>
2020-05-12 16:43:09 +0700

    add splash, signin and signup screen

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import React, { useEffect }  from 'react';
+import { 
+  View, 
+  ActivityIndicator
+} from 'react-native';
+import { 
+  NavigationContainer,
+  DefaultTheme as NavigationDefaultTheme,
+  DarkTheme as NavigationDarkTheme
+} from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 
+import { 
+  Provider as PaperProvider, 
+  DefaultTheme as PaperDefaultTheme,
+  DarkTheme as PaperDarkTheme 
+} from 'react-native-paper';
+
 import Icon from 'react-native-vector-icons/Ionicons';
+import AsyncStorage from '@react-native-community/async-storage';
 
 import HomeScreen from './src/pages/HomeScreen';
 import DetailScreen from './src/pages/DetailScreen';
@@ -13,7 +28,11 @@ import ProfileScreen from './src/pages/ProfileScreen';
 import SupportScreen from './src/pages/SupportScreen';
 import BookmarkScreen from './src/pages/BookmarkScreen';
 import SettingsScreen from './src/pages/SettingsScreen';
+import SplashScreen from './src/pages/SplashScreen';
+import SignInScreen from './src/pages/SignInScreen';
+import SignUpScreen from './src/pages/SignUpScreen';
 import { DrawerContent } from './src/pages/DrawerContent';
+import { AuthContext } from './src/component/context';
 
 const HomeStack = createStackNavigator();
 const DetailStack = createStackNavigator();
@@ -141,6 +160,16 @@ const DetailStackRouter = ({ navigation }) => {
   );
 }
 
+const RootStack = createStackNavigator();
+
+const RootStackRouter = ({navigation}) => (
+    <RootStack.Navigator headerMode='none'>
+        <RootStack.Screen name="SplashScreen" component={SplashScreen}/>
+        <RootStack.Screen name="SignInScreen" component={SignInScreen}/>
+        <RootStack.Screen name="SignUpScreen" component={SignUpScreen}/>
+    </RootStack.Navigator>
+);
+
 function DrawerRouter() {
   return (
     <Drawer.Navigator 
@@ -167,11 +196,144 @@ function DrawerRouter() {
   );
 }
 
-function App () {
+const App = () => {
+
+  const [isDarkTheme, setIsDarkTheme] = React.useState(false);
+
+  const initialLoginState = {
+    isLoading: true,
+    userName: null,
+    userToken: null,
+  };
+
+  const CustomDefaultTheme = {
+    ...NavigationDefaultTheme,
+    ...PaperDefaultTheme,
+    colors: {
+      ...NavigationDefaultTheme.colors,
+      ...PaperDefaultTheme.colors,
+      background: '#ffffff',
+      text: '#333333'
+    }
+  }
+  
+  const CustomDarkTheme = {
+    ...NavigationDarkTheme,
+    ...PaperDarkTheme,
+    colors: {
+      ...NavigationDarkTheme.colors,
+      ...PaperDarkTheme.colors,
+      background: '#333333',
+      text: '#ffffff'
+    }
+  }
+
+  const theme = isDarkTheme ? CustomDarkTheme : CustomDefaultTheme;
+
+  const loginReducer = (prevState, action) => {
+    switch( action.type ) {
+      case 'RETRIEVE_TOKEN': 
+        return {
+          ...prevState,
+          userToken: action.token,
+          isLoading: false,
+        };
+      case 'LOGIN': 
+        return {
+          ...prevState,
+          userName: action.id,
+          userToken: action.token,
+          isLoading: false,
+        };
+      case 'LOGOUT': 
+        return {
+          ...prevState,
+          userName: null,
+          userToken: null,
+          isLoading: false,
+        };
+      case 'REGISTER': 
+        return {
+          ...prevState,
+          userName: action.id,
+          userToken: action.token,
+          isLoading: false,
+        };
+    }
+  };
+
+  const [loginState, dispatch] = React.useReducer(loginReducer, initialLoginState);
+
+  const authContext = React.useMemo(() => ({
+    signIn: async(foundUser) => {
+      // setUserToken('fgkj');
+      // setIsLoading(false);
+      const userToken = String(foundUser[0].userToken);
+      const userName = foundUser[0].username;
+      
+      try {
+        await AsyncStorage.setItem('userToken', userToken);
+      } catch(e) {
+        console.log(e);
+      }
+      // console.log('user token: ', userToken);
+      dispatch({ type: 'LOGIN', id: userName, token: userToken });
+    },
+    signOut: async() => {
+      // setUserToken(null);
+      // setIsLoading(false);
+      try {
+        await AsyncStorage.removeItem('userToken');
+      } catch(e) {
+        console.log(e);
+      }
+      dispatch({ type: 'LOGOUT' });
+    },
+    signUp: () => {
+      // setUserToken('fgkj');
+      // setIsLoading(false);
+    },
+    toggleTheme: () => {
+      setIsDarkTheme( isDarkTheme => !isDarkTheme );
+    }
+  }), []);
+
+  useEffect(() => {
+    setTimeout(async() => {
+      // setIsLoading(false);
+      let userToken;
+      userToken = null;
+      try {
+        userToken = await AsyncStorage.getItem('userToken');
+      } catch(e) {
+        console.log(e);
+      }
+      // console.log('user token: ', userToken);
+      dispatch({ type: 'RETRIEVE_TOKEN', token: userToken });
+    }, 1000);
+  }, []);
+
+  if( loginState.isLoading ) {
+    return(
+      <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+        <ActivityIndicator size="large"/>
+      </View>
+    );
+  }
+
   return (
-    <NavigationContainer>
-      <DrawerRouter/>
-    </NavigationContainer>
+    <PaperProvider theme={theme}>
+      <AuthContext.Provider value={authContext}>
+        <NavigationContainer theme={theme}>
+          { loginState.userToken !== null ? (
+            <DrawerRouter/>
+          )
+        :
+          <RootStackRouter/>
+        }
+        </NavigationContainer>
+      </AuthContext.Provider>
+    </PaperProvider>
   );
 };
 
